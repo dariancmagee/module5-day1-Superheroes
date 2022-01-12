6abb355f71fdb2ea4cba77ef85d024d55647d855
dariancmagee <dariancmagee@gmail.com>
2022-01-11 19:50:10 -0500

    added js

---
+++ script.js
@@ -0,0 +1,7 @@
+const key = `f2e92d2e`;
+
+const moviesList = `http//:www.omdbapi.com/?s=batman&apikey=${key}`;
+
+const detailsList = `http://www.omdbapi.com/?i=insertSelectedimdbIDhere&apikey=${key}`;
+
+
